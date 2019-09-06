cee122fe6ca9ddcff1fb57a4d1d7d4b08c73f8f0
Vlad Buryak <vladislavburyak00@gmail.com>
2019-09-06 17:17:11 +0300

    [! MAJOR !] Rebuild app acc. to Clean Architecture

---
+++ DailerApp/DailerApp.Infrastructure/Services/Realization/DbWriter.cs
@@ -0,0 +1,41 @@
+using System;
+using DailerApp.Data;
+using DailerApp.Services;
+using System.Linq;
+using DailerApp.Models;
+using DailerApp.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using DailerApp.AppCore.Services;
+
+namespace DailerApp.Services
+{
+    public class DbWriter<T>: DbHendler<T>, IDbWriter<T> where T: class
+    {
+        public DbWriter(ApplicationDbContext db): base (db)
+        {
+               
+        }
+
+        public void DeleteAllFromDb()
+        {
+            foreach(var item in dbSet)
+            {
+                dbSet.Remove(item);
+            }
+            _db.SaveChanges();
+        }
+
+        public void DeleteFromDb(T item)
+        {
+            dbSet.Remove(item);
+            _db.SaveChanges();
+        }
+
+        public void WriteToDb(T item)
+        {
+            dbSet.Add(item);
+            _db.SaveChanges();
+            Console.WriteLine($"Written type was: {typeof(T)}");
+        }
+    }
+}
